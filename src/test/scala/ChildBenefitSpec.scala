cb00b4595f4fced640bff2ff5fdb61365e873106
Andrew Boyce <andrew.boyce96@hotmail.com>
2025-05-22 16:33:49 +0100

    TC01,02 and 03 but 03 is failing

--- src/test/scala/ChildBenefitSpec.scala
+++ src/test/scala/ChildBenefitSpec.scala
@@ -14,4 +14,46 @@ class ChildBenefitSpec extends AnyWordSpec with Matchers {
       }    
     }  
   }
+  "TC01" should {
+    "return £60.55" when {
+      "income is £30,000 and the children are 18 in AE, 15, and 8" in {
+        val child1 = ChildInFamily(age = 18, inEducation = true, isDisabled = false)
+        val child2 = ChildInFamily(age = 15, inEducation = false, isDisabled = false)
+        val child3 = ChildInFamily(age = 8, inEducation = false, isDisabled = false)
+        val family = List(child1, child2, child3)
+        val income = 30000
+        val result = ChildBenefit.calculateWeeklyAmount(family, income)
+        val expectedResult = 60.55
+        result shouldBe expectedResult
+      }
+    }
+  }
+  "TC02" should {
+    "return £2.88" when {
+      "income is £55,000 and the children are 20 and 15" in {
+        val child1 = ChildInFamily(age = 20, inEducation = false, isDisabled = false)
+        val child2 = ChildInFamily(age = 15, inEducation = false, isDisabled = false)
+        val family = List(child1, child2)
+        val income = 55000
+        val result = ChildBenefit.calculateWeeklyAmount(family, income)
+        val expectedResult = 2.88
+        result shouldBe expectedResult
+      }
+    }
+  }
+  "TC03" should {
+    "return £11.54" when {
+      "income is £75,000 and the children are 18 not in AE, 16 in AE and 13" in {
+        val child1 = ChildInFamily(age = 18, inEducation = false, isDisabled = false)
+        val child2 = ChildInFamily(age = 16, inEducation = true, isDisabled = false)
+        val child3 = ChildInFamily(age = 13, inEducation = false, isDisabled = false)
+        val family = List(child1, child2)
+        val income = 75000
+        val result = ChildBenefit.calculateWeeklyAmount(family, income)
+        val expectedResult = 11.54
+        result shouldBe expectedResult
+      }
+    }
+  }
 }
+
